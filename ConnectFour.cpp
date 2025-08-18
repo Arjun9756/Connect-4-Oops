a9af2f7e6d5025d40f4c6ada5f7031d7e79e04d4
TruthGuard Deployer <ab9604783@gmail.com>
2025-08-18 20:35:18 +0530

    Fixed range error

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ii , jj; // Global Variables
+int ii, jj; // Global Variables
 
-pair<int,int>canFilledOnCol(vector<vector<string>>&matrix , int &colNumber , bool &isPlayerOne)
+pair<int, int> canFilledOnCol(vector<vector<string>> &matrix, int &colNumber, bool &isPlayerOne)
 {
-    for(int i=matrix.size()-1 ; i>=0 ; i--)
+    for (int i = matrix.size() - 1; i >= 0; i--)
     {
-        if(matrix[i][colNumber] == ".")   // if it is an empty Cells
+        if (matrix[i][colNumber] == ".") // if it is an empty Cells
         {
             matrix[i][colNumber] = (isPlayerOne == true ? "X" : "O");
-            return make_pair(i , colNumber);
+            return make_pair(i, colNumber);
         }
     }
-    return make_pair(-1 , -1);
+    return make_pair(-1, -1);
 }
 
-void printBoard(vector<vector<string>>&matrix) {
+void printBoard(vector<vector<string>> &matrix)
+{
     cout << "\nCurrent Board:\n";
-    for(int i=0; i<ii; i++) {
-        for(int j=0; j<jj; j++) {
+    for (int i = 0; i < ii; i++)
+    {
+        for (int j = 0; j < jj; j++)
+        {
             cout << matrix[i][j] << " ";
         }
         cout << "\n";
@@ -28,73 +31,75 @@ void printBoard(vector<vector<string>>&matrix) {
     cout << "-----------------\n";
 }
 
-inline bool isValid(int i , int j)
-{return (i >= 0 and j >= 0 and i < ii and j < jj);}
+inline bool isValid(int i, int j)
+{
+    return (i >= 0 and j >= 0 and i < ii and j < jj);
+}
 
-bool canUpwardPossible(vector<vector<string>>&matrix , int i , int j)
+bool canUpwardPossible(vector<vector<string>> &matrix, int i, int j)
 {
     string prev = matrix[i][j];
     int count = 0;
 
-    for(int k=i ; k>=0 ; k--)
+    for (int k = i; k >= 0; k--)
     {
-        if(!isValid(k , j) or matrix[k][j] != prev) 
+        if (!isValid(k, j) or matrix[k][j] != prev)
             return false;
-        if(isValid(k , j) and matrix[k][j] == prev)
+        if (isValid(k, j) and matrix[k][j] == prev)
             count++;
-        if(count >= 4)
+        if (count >= 4)
             return true;
     }
     return (count >= 4 ? true : false);
 }
 
-bool canDownwardPossible(vector<vector<string>>&matrix , int i , int j)
+bool canDownwardPossible(vector<vector<string>> &matrix, int i, int j)
 {
     string prev = matrix[i][j];
     int count = 0;
 
-    for(int k=i ; k<matrix.size() ; k++)
+    for (int k = i; k < matrix.size(); k++)
     {
-        if(!isValid(k , j) or matrix[k][j] != prev)
+        if (!isValid(k, j) or matrix[k][j] != prev)
             return false;
-        if(isValid(k , j) and matrix[k][j] == prev)
+        if (isValid(k, j) and matrix[k][j] == prev)
             count++;
-        if(count >= 4)
+        if (count >= 4)
             return true;
     }
     return (count >= 4 ? true : false);
 }
 
-bool canRightwardPossible(vector<vector<string>>&matrix , int i , int j)
+bool canRightwardPossible(vector<vector<string>> &matrix, int i, int j)
 {
     string prev = matrix[i][j];
     int count = 0;
 
-    for(int k=j ; k<matrix[0].size() ; k++)
+    for (int k = j; k < matrix[0].size(); k++)
     {
-        if(!isValid(i , k) or matrix[i][k] != prev)
+        if (!isValid(i, k) or matrix[i][k] != prev)
             return false;
-        if(isValid(i , k) and matrix[i][k] == prev)
+        if (isValid(i, k) and matrix[i][k] == prev)
             count++;
-        if(count >= 4)
+        if (count >= 4)
             return true;
     }
 
     return (count >= 4 ? true : false);
 }
 
-bool canLeftWardPossible(vector<vector<string>>&matrix , int i , int j)
+bool canLeftWardPossible(vector<vector<string>> &matrix, int i, int j)
 {
     string prev = matrix[i][j];
     int count = 0;
 
-    for(int k=j ; k>=0 ; k--)
+    for (int k = j; k >= 0; k--)
     {
-        if(!isValid(i , k) or matrix[i][k] != prev)
+        if (!isValid(i, k) or matrix[i][k] != prev)
             return false;
-        if(isValid(i , k) and matrix[i][k] == prev)
+        if (isValid(i, k) and matrix[i][k] == prev)
             count++;
-        if(count >= 4)
+        if (count >= 4)
             return true;
     }
 
@@ -102,119 +107,152 @@ bool canLeftWardPossible(vector<vector<string>>&matrix , int i , int j)
 }
 
 // Code to Be Cange Two Functions
-bool canLeftDigoPossible(vector<vector<string>>&matrix , int i , int j) {
+bool canLeftDigoPossible(vector<vector<string>> &matrix, int i, int j)
+{
     string prev = matrix[i][j];
     int count = 0;
 
     // upar-left taraf
-    for(int k=0 ; i-k>=0 && j-k>=0 ; k++) {
-        if(matrix[i-k][j-k] != prev) break;
+    for (int k = 0; i - k >= 0 && j - k >= 0; k++)
+    {
+        if (matrix[i - k][j - k] != prev)
+            break;
         count++;
     }
 
     // neeche-right taraf
-    for(int k=1 ; i+k<ii && j+k<jj ; k++) {
-        if(matrix[i+k][j+k] != prev) break;
+    for (int k = 1; i + k < ii && j + k < jj; k++)
+    {
+        if (matrix[i + k][j + k] != prev)
+            break;
         count++;
     }
 
     return count >= 4;
 }
 
-bool canRightDigoPossible(vector<vector<string>>&matrix , int i , int j) {
+bool canRightDigoPossible(vector<vector<string>> &matrix, int i, int j)
+{
     string prev = matrix[i][j];
     int count = 0;
 
     // upar-right taraf
-    for(int k=0 ; i-k>=0 && j+k<jj ; k++) {
-        if(matrix[i-k][j+k] != prev) break;
+    for (int k = 0; i - k >= 0 && j + k < jj; k++)
+    {
+        if (matrix[i - k][j + k] != prev)
+            break;
         count++;
     }
 
     // neeche-left taraf
-    for(int k=1 ; i+k<ii && j-k>=0 ; k++) {
-        if(matrix[i+k][j-k] != prev) break;
+    for (int k = 1; i + k < ii && j - k >= 0; k++)
+    {
+        if (matrix[i + k][j - k] != prev)
+            break;
         count++;
     }
 
     return count >= 4;
 }
 
-
-pair<bool,bool>canThePlayerWins(vector<vector<string>>&matrix , pair<int,int>&nodes , bool &isPlayerOne)
+pair<bool, bool> canThePlayerWins(vector<vector<string>> &matrix, pair<int, int> &nodes, bool &isPlayerOne)
 {
-    // Check for UpWard 
-    bool answerFromUpward = canUpwardPossible(matrix , nodes.first , nodes.second);
-    bool answerFromDownward = canDownwardPossible(matrix , nodes.first , nodes.second);
-    bool answerFromRight = canRightwardPossible(matrix , nodes.first , nodes.second);
-    bool answerFromLeft = canLeftWardPossible(matrix , nodes.first , nodes.second);
-    bool answerFromLeftDigonal = canLeftDigoPossible(matrix , nodes.first , nodes.second);
-    bool answerFromRightDigonal = canRightDigoPossible(matrix , nodes.first , nodes.second);
+    // Check for UpWard
+    bool answerFromUpward = canUpwardPossible(matrix, nodes.first, nodes.second);
+    bool answerFromDownward = canDownwardPossible(matrix, nodes.first, nodes.second);
+    bool answerFromRight = canRightwardPossible(matrix, nodes.first, nodes.second);
+    bool answerFromLeft = canLeftWardPossible(matrix, nodes.first, nodes.second);
+    bool answerFromLeftDigonal = canLeftDigoPossible(matrix, nodes.first, nodes.second);
+    bool answerFromRightDigonal = canRightDigoPossible(matrix, nodes.first, nodes.second);
 
     bool finalAnswer = (answerFromUpward or answerFromDownward or answerFromRight or answerFromLeft or answerFromLeftDigonal or answerFromRightDigonal);
-    if(isPlayerOne)
-        return {finalAnswer , false};
+    if (isPlayerOne)
+        return {finalAnswer, false};
 
-    return {false , finalAnswer};
+    return {false, finalAnswer};
 }
 
-bool whoWons(vector<vector<string>>&matrix)
+bool whoWons(vector<vector<string>> &matrix)
 {
     int moves = 0;
     bool isPlayerOne = true;
 
-    while(moves < matrix.size() * matrix[0].size())
+    while (moves < matrix.size() * matrix[0].size())
     {
         // Take Input from Player
-        pair<int,int>nodes;         // Stores row & col of filled cell
+        pair<int, int> nodes; // Stores row & col of filled cell
         int colNumber = -1;
 
-        if(isPlayerOne == true){
-            while(colNumber == -1){
-                cout<<"Player 1 Chance Enter the Column Number You Want to Drop the Disc Between 0--6 \n";
-                cin>>colNumber;
-                
-                if(colNumber < 0 or colNumber >= 7){
-                    colNumber = -1;
-                }
+        if (isPlayerOne == true)
+        {
+            while (colNumber == -1)
+            {
+                cout << "Player 1 Chance Enter the Column Number You Want to Drop the Disc Between 0--6 \n";
+                cin >> colNumber;
 
-                nodes = canFilledOnCol(matrix , colNumber , isPlayerOne);
-                if(nodes.first == -1 or nodes.second == -1){
-                    cout<<"Please Choose a Distinct Column this Column gets Full \n";
+                if (colNumber < 0 or colNumber >= 7)
+                {
                     colNumber = -1;
+                    cout<<"Give Index Under The Range \n";
+                }
+                else
+                {
+                    nodes = canFilledOnCol(matrix, colNumber, isPlayerOne);
+                    if (nodes.first == -1 or nodes.second == -1)
+                    {
+                        cout << "Please Choose a Distinct Column this Column gets Full \n";
+                        colNumber = -1;
+                    }
                 }
             }
         }
-        else{
-            while(colNumber == -1){
-                cout<<"Player 2 Chance Enter the Column Number You Want To Drop the Disc Between 0--6\n";
-                cin>>colNumber;
+        else
+        {
+            while (colNumber == -1)
+            {
+                cout << "Player 2 Chance Enter the Column Number You Want To Drop the Disc Between 0--6\n";
+                cin >> colNumber;
 
-                if(colNumber < 0 or colNumber >= 7){
+                if (colNumber < 0 or colNumber >= 7)
+                {
                     colNumber = -1;
+                    cout<<"Give Index Under The Range \n";
                 }
-
-                nodes = canFilledOnCol(matrix , colNumber , isPlayerOne);
-                if(nodes.first == -1 or nodes.second == -1){
-                    cout<<"Please Choose a Distinct Column this Column gets Full \n";
-                    colNumber = -1;
+                else
+                {
+                    nodes = canFilledOnCol(matrix, colNumber, isPlayerOne);
+                    if (nodes.first == -1 or nodes.second == -1)
+                    {
+                        cout << "Please Choose a Distinct Column this Column gets Full \n";
+                        colNumber = -1;
+                    }
                 }
             }
         }
-        
+
         printBoard(matrix);
-    
-        pair<bool,bool>winsNodes = canThePlayerWins(matrix , nodes , isPlayerOne);
-        if(winsNodes.first == true){
+
+        pair<bool, bool> winsNodes = canThePlayerWins(matrix, nodes, isPlayerOne);
+        if (winsNodes.first == true)
+        {
             return 1; // 1st Player wins
         }
-        else if(winsNodes.second == true){
+        else if (winsNodes.second == true)
+        {
             return 2; // 2nd Player Wins
-        }  
+        }
 
-        if(winsNodes.first) { printBoard(matrix); return 1; }
-        if(winsNodes.second) { printBoard(matrix); return 2; }
-        
+        if (winsNodes.first)
+        {
+            printBoard(matrix);
+            return 1;
+        }
+        if (winsNodes.second)
+        {
+            printBoard(matrix);
+            return 2;
+        }
+
         isPlayerOne = !isPlayerOne;
         moves++;
     }
@@ -224,12 +262,15 @@ bool whoWons(vector<vector<string>>&matrix)
 
 int main()
 {
-    vector<vector<string>>matrix(6 , vector<string>(7 , "."));
+    vector<vector<string>> matrix(6, vector<string>(7, "."));
     ii = matrix.size();
     jj = matrix[0].size();
 
     int result = whoWons(matrix);
-    if(result == 1) cout<<"Player 1 wins!\n";
-    else if(result == 2) cout<<"Player 2 wins!\n";
-    else cout<<"Match draw!\n";
+    if (result == 1)
+        cout << "Player 1 wins!\n";
+    else if (result == 2)
+        cout << "Player 2 wins!\n";
+    else
+        cout << "Match draw!\n";
 }
